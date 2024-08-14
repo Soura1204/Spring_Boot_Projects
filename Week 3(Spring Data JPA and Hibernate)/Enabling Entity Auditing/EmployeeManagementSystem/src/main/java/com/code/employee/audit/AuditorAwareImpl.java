0b2651939be3eacc4de888e308f218dd792f8bb0
Sourajit Datta <91087993+Soura1204@users.noreply.github.com>
2024-08-15 00:46:42 +0530

    Add files via upload

---
+++ Week 3(Spring Data JPA and Hibernate)/Enabling Entity Auditing/EmployeeManagementSystem/src/main/java/com/code/employee/audit/AuditorAwareImpl.java
@@ -0,0 +1,14 @@
+package com.code.employee.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+
+public class AuditorAwareImpl implements AuditorAware<String>{
+	
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of("Sourajit"); //just for testing
+	}
+
+}
